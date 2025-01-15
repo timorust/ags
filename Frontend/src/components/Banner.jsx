a1d44e0fd2922cfb311079d5b9ab324e164b2bf5
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-15 05:46:38 +0000

    bordr

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -16,7 +16,7 @@ function Banner() {
 							et totam. Tempora amet atque expedita, quae corrupti totam sed
 							pariatur corporis at veniam est voluptas animi!
 						</p>
-						<label className='input input-bordered flex items-center gap-2 dark:bg-slate-900 dark:text-white'>
+						<label className='input input-bordered flex items-center gap-2 dark:bg-slate-900 dark:text-white border-blue-500 focus:border-blue-700 rounded-md px-2 py-1'>
 							<svg
 								xmlns='http://www.w3.org/2000/svg'
 								viewBox='0 0 16 16'
@@ -26,7 +26,7 @@ function Banner() {
 								<path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
 								<path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
 							</svg>
-							<input type='text' className='flex-grow outline-none border-blue-500 border-2 focus:border-blue-700 rounded-md px-2 py-1 dark:bg-slate-900 dark:text-white' placeholder='Email' />
+							<input type='text' className='flex-grow outline-none dark:bg-slate-900 dark:text-white' placeholder='Email' />
 						</label>
 					</div>
 					<button className='btn mt-6 bg-green-500 text-white hover:bg-green-700'>
