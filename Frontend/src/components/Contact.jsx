dee2a001ecedf6b06e711344f89711fec6c3e277
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-15 19:27:27 +0000

    user_id

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -17,7 +17,7 @@ function SendEmail() {
 
 		const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID
 		const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID
-		const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+		const publicKey = process.env.REACT_APP_EMAILJS_USER_ID
 
 		emailjs.sendForm(serviceId, templateId, form.current, publicKey).then(
 			() => {
