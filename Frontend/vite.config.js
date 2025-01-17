886931b3d583b533cda5b1d75ab18d7dc6f2bc31
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-17 09:21:53 +0000

    auto

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true,
+    },
     proxy: {
       '/conference': {
         target: 'http://localhost:4002',
