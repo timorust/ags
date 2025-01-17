1e489a266d2ae229b761f35d803c104176caabd2
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-17 13:31:17 +0000

    link-href

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,13 +1,15 @@
+import { Link } from 'react-router-dom';
+
 function Footer() {
 	return (
 		<div>
 			<hr />
 			<footer className='footer footer-center p-10 text-base-content rounded dark:bg-slate-900 dark:text-white'>
 				<nav className='grid grid-flow-col gap-4'>
-					<a className='link link-hover'>About us</a>
-					<a className='link link-hover'>Contact</a>
-					<a className='link link-hover'>Jobs</a>
-					<a className='link link-hover'>Press kit</a>
+					<Link className='link link-hover' to='/about'>About us</Link>
+					<Link className='link link-hover' to='/contact'>Contact</Link>
+					<Link className='link link-hover' to='/'>Home</Link>
+					<Link className='link link-hover' to='/registration'>Registration</Link>
 				</nav>
 				<nav>
 					<div className='grid grid-flow-col gap-4'>
