251eb426fbfa24f2ba533943267dc77d5276bbb0
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-18 12:09:57 +0000

    text-black

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -11,7 +11,7 @@ function Banner() {
 						<h1 className='text-2xl md:text-4xl font-bold'>
 							{t('The Azerbaijan Glaucoma Society')}
 						</h1>
-						<p className='text-sm md:text-xl'>
+						<p className='text-sm md:text-xl text-black'>
 						{t('Description')}
 						</p>
 						<label className='flex items-center gap-2 dark:bg-slate-900 dark:text-white border-2 border-blue-500 focus-within:border-blue-700 rounded-md px-2 py-1'>
