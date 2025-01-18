496a1de893986710b47fb8f3371cea80a4e10d2d
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-18 15:35:03 +0000

    corr-image

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -18,26 +18,6 @@ function Banner() {
 dark:text-gray-300 font-light">
 							{t('Description')}
 						</p>
-
-
-						{/* <label className='flex items-center gap-2 dark:bg-slate-900 dark:text-white border-2 border-blue-500 focus-within:border-blue-700 rounded-md px-2 py-1'>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								viewBox='0 0 16 16'
-								fill='currentColor'
-								className='w-4 h-4 opacity-70'
-							>
-								<path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
-								<path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
-							</svg>
-							<input
-								type='text'
-								className='flex-grow outline-none border-none bg-transparent dark:text-white'
-								placeholder='Email'
-							/>
-						</label> */}
-
-
 					</div>
 					<div className='grid grid-flow-col gap-4 mt-10 pt-10'>
 						{/* Twitter */}
@@ -111,13 +91,12 @@ dark:text-gray-300 font-light">
 					</button> */}
 				</div>
 				<div className='order-1 w-full mt-20 md:w-1/2 flex justify-center md:justify-start overflow-hidden mr-10 ml-10 pt-10 pb-10 pr-10'>
-					<img
+					 <img
 						src={banner}
-						alt='Banner'
-						className='w-[90%] max-w-[450px] md:w-[550px] md:h-[460px] md:ml-12 object-contain rounded-lg animate-slideIn transition-transform duration-1000 ease-in-out'
+						alt='banner'
+						className='w-full h-full object-cover'
 					/>
 				</div>
-
 			</div>
 		</>
 	)
