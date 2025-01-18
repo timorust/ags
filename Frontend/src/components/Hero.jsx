09df4f0222354abcaab1b2fb8d34926cbc5f995a
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-18 09:10:43 +0000

    nbg

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <>
       <div className='max-w-screen-2xl container mx-auto md:px-20 px-6 flex flex-col md:flex-row my-10'>
         <div className="hero h-96 md:h-[500px] rounded-box overflow-hidden" style={{ backgroundImage: `url("https://www.akupunktur-noll.de/wp-content/uploads/2016/08/short-sightedness-myopia-is-considered-by-specialists-to-be-a-cause-of-glaucoma-en.png")` }}>
-          <div className="hero-overlay bg-opacity-60 bg-blue-100"></div>
+          <div className="hero-overlay bg-opacity-60"></div>
           <div className="hero-content text-center text-secondary-content">
             <div className="max-w-lg">
               <h1 className="mb-5 sm:mb-7 text-4xl sm:text-5xl font-bold">
