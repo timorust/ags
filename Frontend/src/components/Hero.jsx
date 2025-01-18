2d22243fac6d6054490e180b92924d10d19764c2
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-18 19:11:19 +0000

    hero-trnsl

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next'
+
 
 export default function Hero() {
+
+  const { t } = useTranslation()
   return (
     <>
       <div className='max-w-screen-2xl container mx-auto md:px-20 px-6 flex flex-col md:flex-row my-10'>
@@ -9,10 +13,10 @@ export default function Hero() {
           <div className="hero-content text-center text-secondary-content">
             <div className="max-w-lg">
               <h1 className="mb-5 sm:mb-7 text-4xl sm:text-5xl font-bold dark:text-black">
-                Azerbaijan Glaucoma Society
+                {t('The Azerbaijan Glaucoma Society')}
               </h1>
               <p className="mb-5 sm:mb-7 sm:text-lg dark:text-black">
-                Join us for an insightful conference on the latest advancements in glaucoma research and treatment. Together, let's make strides in eye health.
+              {t('Join us for an insightful conference on the latest advancements in glaucoma research and treatment. Together, let`s make strides in eye health.')}
               </p>
 
               {/* <button className="btn btn-warning sm:btn-wide">Get Started</button> */}
