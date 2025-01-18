4262aa81a2a4633e2faf07b3e62b8474bd67b252
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-18 19:37:17 +0000

    log-pink

--- Frontend/src/components/Logout.jsx
+++ Frontend/src/components/Logout.jsx
@@ -25,7 +25,7 @@ function Logout() {
 	return (
 		<div>
 			<button
-				className='px-3 py-2 bg-red-500 text-white rounded-md cursor-pointer'
+				className='px-3 py-2 bg-pink-500 text-white rounded-md cursor-pointer'
 				onClick={handleLogout}
 			>
 				{t('Logout')}
