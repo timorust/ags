809efcb988d37c445a41f059724fca6be95169ce
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-19 21:45:34 +0000

    banner-x

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -20,40 +20,22 @@ dark:text-gray-300 font-light">
 						</p>
 
 
-						{/* <label className='flex items-center gap-2 dark:bg-slate-900 dark:text-white border-2 border-blue-500 focus-within:border-blue-700 rounded-md px-2 py-1'>
-							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								viewBox='0 0 16 16'
-								fill='currentColor'
-								className='w-4 h-4 opacity-70'
-							>
-								<path d='M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z' />
-								<path d='M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z' />
-							</svg>
-							<input
-								type='text'
-								className='flex-grow outline-none border-none bg-transparent dark:text-white'
-								placeholder='Email'
-							/>
-						</label> */}
-
-
 					</div>
 					<div className='grid grid-flow-col gap-4 mt-10 pt-10'>
 						{/* Twitter */}
 						<a
-							href='https://twitter.com'
-							target='_blank'
-							rel='noopener noreferrer'
+							href="https://x.com/glaucomaaz"
+							target="_blank"
+							rel="noopener noreferrer"
 						>
 							<svg
-								xmlns='http://www.w3.org/2000/svg'
-								width='24'
-								height='24'
-								viewBox='0 0 24 24'
-								className='fill-current'
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								className="fill-current"
 							>
-								<path d='M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z'></path>
+								<path d="M4 4L20 20M20 4L4 20" stroke="currentColor" strokeWidth="2" />
 							</svg>
 						</a>
 						{/* YouTube */}
