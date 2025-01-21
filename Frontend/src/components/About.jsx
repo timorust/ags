6129ac29d1a6d785a30eb4911dcad544b52c9a9b
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-21 01:02:45 +0000

    corr-img1

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -24,8 +24,9 @@ function About() {
 	const images = [
 		'https://scontent.ftlv5-1.fna.fbcdn.net/v/t1.6435-9/54519863_395658020985029_4037793252840046592_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=JIKGAE8H4bMQ7kNvgGWCaLG&_nc_zt=23&_nc_ht=scontent.ftlv5-1.fna&_nc_gid=ApLVVzSKmpRAICfkCQ925cB&oh=00_AYBMK3lYvWCTMMwN8lEHbC_yuiX0MyWQnuLJbyYpfUNzFQ&oe=67B65E79',
 		'https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/473041101_1711674712716680_4082012573429841953_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=8nnKhtFSkAMQ7kNvgF6kd1e&_nc_zt=23&_nc_ht=scontent.ftlv5-1.fna&_nc_gid=ALJVBRKJCk5pXZNPoK-_G7g&oh=00_AYAaofgIPmXvKzAjMpMTxrFGJV4Q1K6t27DQTckkWoXaHA&oe=6794A8A6',
-		'https://scontent.ftlv5-1.fna.fbcdn.net/v/t1.6435-9/53649948_395656340985197_4848800296572289024_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_ohc=mtVyY901VuAQ7kNvgE-u0QZ&_nc_zt=23&_nc_ht=scontent.ftlv5-1.fna&_nc_gid=AqGpnKQv6kwV5QHujxubaD0&oh=00_AYDBiYQumeI8pQWq1RdOKmmuce6wvUhIYTepQWBE07SqpQ&oe=67B64690',
 		'https://scontent.ftlv5-1.fna.fbcdn.net/v/t39.30808-6/473117477_1711674109383407_6099483338691208202_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=833d8c&_nc_ohc=thnwNFEysrkQ7kNvgEq_R_o&_nc_zt=23&_nc_ht=scontent.ftlv5-1.fna&_nc_gid=AuQJq1gNKOflgYfmD3O1y1o&oh=00_AYD0VwWDTa9UtjMylnFZEmtMur_V9llfS3m0YOrefFLAwA&oe=6794D7DA',
+		'https://scontent.ftlv5-1.fna.fbcdn.net/v/t1.6435-9/54255698_395658477651650_5148660954535297024_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=PVlFPdpVwnwQ7kNvgHE2z0K&_nc_zt=23&_nc_ht=scontent.ftlv5-1.fna&_nc_gid=A5YENtyk7dw7OeZS-h2nGSY&oh=00_AYDPNlhQ624YQKmAKcPjySqQ2GzmiKm0OE02UJvsl-Hipg&oe=67B673E4',
+
 	]
 
 	useEffect(() => {
@@ -45,11 +46,11 @@ function About() {
 	return (
 		<>
 			<Navbar />
-			<CountdownTimer targetDate='2025-03-15T09:00:00'/>
+			<CountdownTimer targetDate='2025-03-15T09:00:00' />
 			<div className='max-w-screen-2xl container mx-auto px-6 md:px-20'>
 				<div className='text-center mb-12'>
 					<h1 className='text-3xl md:text-5xl font-semibold dark:text-white text-black'>
-					{t('We`re delighted to have you')},
+						{t('We`re delighted to have you')},
 						<span className='text-yellow-500'>{t('Here')}! :)</span>
 					</h1>
 				</div>
@@ -60,9 +61,8 @@ function About() {
 							key={index}
 							src={image}
 							alt={`Image ${index + 1}`}
-							className={`absolute w-full h-auto rounded-t-lg object-cover ${
-								index === imageIndex ? 'opacity-100' : 'opacity-0'
-							}`}
+							className={`absolute w-full h-auto rounded-t-lg object-cover ${index === imageIndex ? 'opacity-100' : 'opacity-0'
+								}`}
 						/>
 					))}
 				</div>
