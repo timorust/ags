f27ffaa56e4f53e14be57dc654ee2a5cd4912413
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-21 02:48:20 +0000

    f1

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -18,11 +18,11 @@ const Cards = ({ item }) => {
   return (
     <div className='dark:bg-slate-900 dark:text-white dark:border card bg-base-100 w-full sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2 2xl:w-1/3 shadow-xl transform transition-transform duration-700 ease-out hover:scale-105 rounded-lg m-2'>
       {/* עיצוב תמונה רספונסיבי */}
-      <figure className='overflow-hidden w-full h-48 sm:h-56 md:h-64 lg:h-72 lg:w-72 xl:h-96 2xl:h-128'>
+      <figure className='overflow-hidden w-full h-48 sm:h-56 md:h-64 lg:h-72 xl:h-96'>
         <img
           src={item.image}
           alt={t(item.name)}
-          className='w-full h-full object-cover rounded-t-lg'
+          className='w-full h-full object-cover rounded-t-lg' 
         />
       </figure>
 
