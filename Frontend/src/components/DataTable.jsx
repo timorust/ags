01c4bff1d3e003a9cc62a3f22571218fca84cb0b
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-26 20:22:20 +0000

    cor3

--- Frontend/src/components/DataTable.jsx
+++ Frontend/src/components/DataTable.jsx
@@ -14,7 +14,7 @@ function DataTable() {
       <CountdownTimer targetDate="2025-03-15T09:00:00" />
       <div className="dark:bg-slate-900 dark:text-white min-h-screen flex flex-col items-center py-8">
         <div 
-          className="flex items-center justify-center h-64 w-64 bg-yellow-500 rounded-full shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer mt-20 dark:bg-slate-900 dark:text-white"
+          className="flex items-center justify-center h-64 w-64 bg-yellow-500 rounded-full shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer mt-20 dark:bg-slate-900 dark:text-yellow"
           onClick={handleIconClick}
         >
           <Lightbulb size={120} className="text-white" />
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -58,7 +58,7 @@ function Navbar() {
         <a href='/registration' className="text-cyan-400 hover:text-cyan-600 active:text-cyan-700 duration-200">{t('Registration')}</a>
       </li>
       <li>
-        <a href='/list' className="text-cyan-400 hover:text-cyan-600 active:text-cyan-700 duration-200">{t('Syllabus')}</a>
+        <a href='/list' className="text-cyan-400 hover:text-cyan-600 active:text-cyan-700 duration-200">{t('Program')}</a>
       </li>
     </>
   )
