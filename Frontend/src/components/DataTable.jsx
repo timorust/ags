f494f2e86a5f62b6cfa609e0894ca33cce0406f0
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-26 08:02:00 +0000

    cordata

--- Frontend/src/components/DataTable.jsx
+++ Frontend/src/components/DataTable.jsx
@@ -5,13 +5,8 @@ import Footer from './Footer';
 
 function DataTable() {
   const data = [
-    { id: 1, name: 'Cy Ganderton', job: 'Quality Control Specialist', topic: 'Blue' },
-    { id: 2, name: 'Hart Hagerty', job: 'Desktop Support Technician', topic: 'Purple' },
-    { id: 3, name: 'Brice Swyre', job: 'Tax Accountant', topic: 'Red' },
-    { id: 4, name: 'Jane Doe', job: 'Software Engineer', topic: 'Green' },
-    { id: 5, name: 'Jane Doe', job: 'Software Engineer', topic: 'Green' },
-    { id: 6, name: 'Jane Doe', job: 'Software Engineer', topic: 'Green' },
-    { id: 7, name: 'Jane Doe', job: 'Software Engineer', topic: 'Green' },
+    { id: 1, name: 'Dr. Jamil Hasanov', job: 'Chairman', topic: 'Zarifa Aliyeva National Eye Centre' },
+    { id: 2, name: 'Dr. Sona IbrahImova PhD', job: 'Vice Chairman', topic: 'Zarifa Aliyeva National Eye Centre' },
   ];
 
   return (
@@ -19,9 +14,9 @@ function DataTable() {
       <Navbar />
       <CountdownTimer targetDate="2025-03-15T09:00:00" />
       <div className="dark:bg-slate-900 dark:text-white min-h-screen flex flex-col items-center py-8">
-        <h2 className="text-2xl font-semibold mb-4">Employee Details</h2>
+        <h2 className="text-2xl font-semibold mb-4"><span className='text-yellow-500'>Information about the lecturers</span></h2>
         <p className="text-gray-600 dark:text-gray-400 mb-6 text-center max-w-md">
-          Below is a list of employee details, including job titles and favorite colors.
+          Discover groundbreaking advancements in glaucoma care with two esteemed specialists. With advanced degrees and extensive clinical expertise, they will share the latest innovations and practices shaping the future of glaucoma management at The Azerbaijan Glaucoma Society conference.
         </p>
 
         <div className="overflow-x-auto w-full max-w-4xl px-4">
@@ -39,7 +34,7 @@ function DataTable() {
                   Lecture Name
                 </th>
                 <th className="px-4 py-2 border border-gray-300 dark:border-gray-700 text-center">
-                  Job
+                  Role
                 </th>
                 <th className="px-4 py-2 border border-gray-300 dark:border-gray-700 text-center">
                   Lecture Topic
@@ -68,9 +63,8 @@ function DataTable() {
 function TableRow({ index, name, job, topic }) {
   return (
     <tr
-      className={`${
-        index % 2 === 0 ? 'bg-gray-100 dark:bg-gray-800' : 'bg-white dark:bg-gray-900'
-      } hover:bg-blue-50 dark:hover:bg-blue-900`}
+      className={`${index % 2 === 0 ? 'bg-gray-100 dark:bg-gray-800' : 'bg-white dark:bg-gray-900'
+        } hover:bg-blue-50 dark:hover:bg-blue-900`}
       role="row"
     >
       <th
