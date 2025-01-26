7f094db4c2d80653aa0f28c9bf68dff28a891f4f
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-26 19:27:54 +0000

    title

--- Frontend/src/components/EventConference.jsx
+++ Frontend/src/components/EventConference.jsx
@@ -99,11 +99,11 @@ function EventConference() {
     <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 py-12">
       <div>
         <h1 className="font-semibold text-2xl md:text-3xl pb-4">
-          <span className="text-yellow-500">Event</span>
+          <span className="text-yellow-500">Glaucoma conferences in 2025</span>
         </h1>
-        <p className="text-gray-600 mb-6">
+        {/* <p className="text-gray-600 mb-6">
           Discover upcoming conferences with exciting opportunities to learn and connect.
-        </p>
+        </p> */}
 
         <div className="slider-container">
           {conference.length === 0 ? (
