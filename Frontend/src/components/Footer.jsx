4adf51443f0160ed25ffe698afbbc8f134d50532
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-27 10:24:31 +0000

    bluefoot

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -8,12 +8,12 @@ function Footer() {
 			<hr />
 			<footer className='footer footer-center p-10 text-base-content rounded dark:bg-slate-900 dark:text-white'>
 				<nav className='grid grid-flow-col gap-4'>
-					<Link className='link link-hover text-cyan-400 hover:text-cyan-600 active:text-cyan-700 duration-200' to='/'>{t('Home')}</Link>
-					<Link className='link link-hover text-cyan-400 hover:text-cyan-600 active:text-cyan-700 duration-200' to='/meeting'>{t('Event')}</Link>
-					<Link className='link link-hover text-cyan-400 hover:text-cyan-600 active:text-cyan-700 duration-200' to='/contact'>{t('Contact')}</Link>
-					<Link className='link link-hover text-cyan-400 hover:text-cyan-600 active:text-cyan-700 duration-200' to='/about'>{t('About')}</Link>
-					<Link className='link link-hover text-cyan-400 hover:text-cyan-600 active:text-cyan-700 duration-200' to='/media'>{t('Media')}</Link>
-					<Link className='link link-hover text-cyan-400 hover:text-cyan-600 active:text-cyan-700 duration-200' to='/registration'>{t('Join')}</Link>
+					<Link className='link link-hover text-blue-300 hover:text-blue-600 active:text-blue-700 duration-200' to='/'>{t('Home')}</Link>
+					<Link className='link link-hover text-blue-300 hover:text-blue-600 active:text-blue-700 duration-200' to='/meeting'>{t('Event')}</Link>
+					<Link className='link link-hover text-blue-300 hover:text-blue-600 active:text-blue-700 duration-200' to='/contact'>{t('Contact')}</Link>
+					<Link className='link link-hover text-blue-300 hover:text-blue-600 active:text-blue-700 duration-200' to='/about'>{t('About')}</Link>
+					<Link className='link link-hover text-blue-300 hover:text-blue-600 active:text-blue-700 duration-200' to='/media'>{t('Media')}</Link>
+					<Link className='link link-hover text-blue-300 hover:text-blue-600 active:text-blue-700 duration-200' to='/registration'>{t('Join')}</Link>
 				</nav>
 				<nav>
 					<div className='grid grid-flow-col gap-4'>
