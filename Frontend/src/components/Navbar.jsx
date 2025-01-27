fa86d56c73b607fea4d7b39941db9d59645fe1dc
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-27 09:30:06 +0000

    md

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -87,11 +87,11 @@ function Navbar() {
               <div
                 tabIndex={0}
                 role='button'
-                className='btn btn-ghost lg:hidden'
+                className='btn btn-ghost md:hidden'
               >
                 <svg
                   xmlns='http://www.w3.org/2000/svg'
-                  className='h-5 w-3'
+                  className='h-5 w-5'
                   fill='none'
                   viewBox='0 0 24 24'
                   stroke='currentColor'
