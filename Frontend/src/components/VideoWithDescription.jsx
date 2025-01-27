01ee5c6c048eeeda9727a139f46e8133507b68c3
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-27 18:03:52 +0000

    sp

--- Frontend/src/components/VideoWithDescription.jsx
+++ Frontend/src/components/VideoWithDescription.jsx
@@ -33,7 +33,7 @@ const VideoWithDescription = () => {
 
 				<div className="flex-1 md:w-1/3 text-left">
 					<h3 className="font-semibold text-2xl md:text-3xl pb-4">
-						Our call to fight glaucoma
+						<span className="text-blue-300">{t('Our call to fight glaucoma')}</span>
 					</h3>
 					<ul className="mb-5 sm:mb-7 sm:text-lg dark:text-black">
 						<li>Early Detection and Regular Screenings !!!</li>
