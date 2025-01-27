8b7c636f00e865edd3e1b0ec58f31a88a75b4818
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-27 19:58:14 +0000

    prgr

--- Frontend/src/components/DataTable.jsx
+++ Frontend/src/components/DataTable.jsx
@@ -1,25 +1,34 @@
-import { Lightbulb } from 'lucide-react'; 
+import { Lightbulb } from 'lucide-react';
 import CountdownTimer from './CountdownTimer';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 function DataTable() {
   const handleIconClick = () => {
-    window.location.href = 'https://drive.google.com/file/d/1p-ONpFgQCFO470vTIg_sgD7Sqwv-Ms3Y/view?usp=drive_link'; 
+    window.location.href = 'https://drive.google.com/file/d/1p-ONpFgQCFO470vTIg_sgD7Sqwv-Ms3Y/view?usp=drive_link';
   };
 
   return (
     <>
       <Navbar />
       <CountdownTimer targetDate="2025-03-15T09:00:00" />
-      <div className="dark:bg-slate-900 dark:text-white min-h-screen flex flex-col items-center py-8">
+      {/* <div className="dark:bg-slate-900 dark:text-white min-h-screen flex flex-col items-center py-8">
         <div 
           className="flex items-center justify-center h-64 w-64 bg-yellow-300 rounded-full shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer mt-20 dark:bg-slate-900"
           onClick={handleIconClick}
         >
           <Lightbulb size={120} className="text-white" />
         </div>
+      </div> */}
+      <div className="dark:bg-slate-900 dark:text-white min-h-screen flex flex-col items-center py-8">
+        <div
+          className="flex items-center justify-center h-64 w-64 bg-yellow-300 rounded-full shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer mt-20 dark:bg-slate-900 border-4 border-gray-600 dark:border-gray-800"
+          onClick={handleIconClick}
+        >
+          <Lightbulb size={120} className="text-white" />
+        </div>
       </div>
+
       <Footer />
     </>
   );
