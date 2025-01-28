4f971aeca6e827a6c33371e51f67bc785c951170
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-28 09:51:39 +0000

    p-text

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -7,52 +7,21 @@ export default function Hero() {
   const { t } = useTranslation()
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-6 flex flex-col md:flex-row my-10">
-        <div
-          className="hero h-96 md:h-[500px] rounded-box overflow-hidden"
-          style={{
-            backgroundImage: `url("https://www.akupunktur-noll.de/wp-content/uploads/2016/08/short-sightedness-myopia-is-considered-by-specialists-to-be-a-cause-of-glaucoma-en.png")`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
+      <div className='max-w-screen-2xl container mx-auto md:px-20 px-6 flex flex-col md:flex-row my-10'>
+        <div className="hero h-96 md:h-[500px] rounded-box overflow-hidden" style={{ backgroundImage: `url("https://www.akupunktur-noll.de/wp-content/uploads/2016/08/short-sightedness-myopia-is-considered-by-specialists-to-be-a-cause-of-glaucoma-en.png")` }}>
           <div className="hero-overlay bg-opacity-60"></div>
-          <div
-            className="hero-content text-center text-secondary-content lg:mr-20"
-          >
+          <div className="hero-content text-center text-secondary-content">
             <div className="max-w-lg">
               <h1 className="mb-5 sm:mb-7 text-4xl sm:text-5xl font-bold dark:text-black">
                 <span className="text-blue-300">{t('Azerbaijan Glaucoma Society')}</span>
               </h1>
-              <p className="mb-5 sm:mb-7 sm:text-lg dark:text-black">
-                <span className="text-white">{t('heroText')}</span>
+              <p className="text-base md:text-lg text-justify leading-relaxed tracking-normal text-gray-500 dark:text-gray-300 font-ligh">
+                {t('heroText')}
               </p>
-
-              {/* <button className="btn btn-warning sm:btn-wide">Get Started</button> */}
             </div>
           </div>
-
         </div>
       </div>
     </>
-    // <>
-    //   <div className='max-w-screen-2xl container mx-auto md:px-20 px-6 flex flex-col md:flex-row my-10'>
-    //     <div className="hero h-96 md:h-[500px] rounded-box overflow-hidden" style={{ backgroundImage: `url("https://www.akupunktur-noll.de/wp-content/uploads/2016/08/short-sightedness-myopia-is-considered-by-specialists-to-be-a-cause-of-glaucoma-en.png")` }}>
-    //       <div className="hero-overlay bg-opacity-60"></div>
-    //       <div className="hero-content text-center text-secondary-content">
-    //         <div className="max-w-lg">
-    //           <h1 className="mb-5 sm:mb-7 text-4xl sm:text-5xl font-bold dark:text-black">
-    //             <span className="text-blue-300">{t('Azerbaijan Glaucoma Society')}</span>
-    //           </h1>
-    //           <p className="mb-5 sm:mb-7 sm:text-lg dark:text-black">
-    //             <span className="text-white">{t('heroText')}</span>
-    //           </p>
-
-    //           {/* <button className="btn btn-warning sm:btn-wide">Get Started</button> */}
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </>
   )
 }
