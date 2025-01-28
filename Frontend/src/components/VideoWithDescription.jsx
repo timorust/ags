ed0eb8fae260b142de9b90dce57d99c6f7eb1850
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-28 10:43:33 +0000

    wcorr

--- Frontend/src/components/VideoWithDescription.jsx
+++ Frontend/src/components/VideoWithDescription.jsx
@@ -6,7 +6,7 @@ const VideoWithDescription = () => {
 	return (
 		<div className='max-w-screen-2xl container mx-auto md:px-20 px-4 mt-20 pt-16 dark:bg-slate-800 dark:text-white'>
 			<div className='flex flex-col md:flex-row items-center space-y-12 md:space-y-0 md:space-x-16 mb-5'>
-				<div className='flex-1 md:w-1/3 text-left mb-10 mr-20'>
+				<div className='flex-1 md:w-1/3 text-left mb-10 mr-10'>
 					<div className='aspect-w-16 aspect-h-9 rounded-lg overflow-hidden shadow-xl'>
 						{/* שימוש בכתובת נכונה להטמעה */}
 						<iframe
@@ -21,7 +21,7 @@ const VideoWithDescription = () => {
 					</div>
 				</div>
 
-				<div className="flex-1 md:w-1/3 text-justify">
+				<div className="flex-1 md:w-1/3 text-justify ml-10">
 					<h3 className="font-semibold text-2xl md:text-3xl pb-4">
 						<span className="text-blue-500">{t('Our call to fight glaucoma')}</span>
 					</h3>
