366b3e75d3124469734943efa7ecfe7bf9f93701
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-28 10:19:00 +0000

    br-2

--- Frontend/src/components/DataTable.jsx
+++ Frontend/src/components/DataTable.jsx
@@ -14,7 +14,7 @@ function DataTable() {
       <CountdownTimer targetDate="2025-03-15T09:00:00" />
       <div className="dark:bg-slate-900 dark:text-white min-h-screen flex flex-col items-center py-8">
         <div
-          className="flex items-center justify-center h-64 w-64 bg-yellow-300 rounded-full shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer mt-20 dark:bg-slate-900 border-4 border-blue-600 dark:border-blue-800"
+          className="flex items-center justify-center h-64 w-64 bg-yellow-300 rounded-full shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer mt-20 dark:bg-slate-900 border-2 border-blue-600 dark:border-blue-800"
           onClick={handleIconClick}
         >
           <Lightbulb size={120} className="text-white" />
