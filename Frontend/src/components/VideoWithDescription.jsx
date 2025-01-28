56aef3646e98afc1c6c312ba6541dce90c62c141
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-28 10:55:27 +0000

    video

--- Frontend/src/components/VideoWithDescription.jsx
+++ Frontend/src/components/VideoWithDescription.jsx
@@ -4,9 +4,9 @@ const VideoWithDescription = () => {
 
 	const { t } = useTranslation()
 	return (
-		<div className='max-w-screen-2xl container mx-auto md:px-20 px-4 mt-20 pt-16 dark:bg-slate-800 dark:text-white'>
+		<div className='max-w-screen-2xl container mx-auto px-4 mt-20 pt-16 dark:bg-slate-800 dark:text-white'>
 			<div className='flex flex-col md:flex-row items-center space-y-12 md:space-y-0 md:space-x-16 mb-5'>
-				<div className='flex-1 md:w-1/3 text-left mb-10 mr-10'>
+				<div className='flex-1 text-left mb-10 mr-10'>
 					<div className='aspect-w-16 aspect-h-9 rounded-lg overflow-hidden shadow-xl'>
 						{/* שימוש בכתובת נכונה להטמעה */}
 						<iframe
@@ -21,7 +21,7 @@ const VideoWithDescription = () => {
 					</div>
 				</div>
 
-				<div className="flex-1 md:w-1/3 text-justify">
+				<div className="flex-1 text-justify">
 					<h3 className="font-semibold text-2xl md:text-3xl pb-4">
 						<span className="text-blue-500">{t('Our call to fight glaucoma')}</span>
 					</h3>
