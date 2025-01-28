c1f382653647faf8386c7031afc5a745da6c232e
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-01-28 06:40:14 +0000

    sp

--- Frontend/src/components/RegisterForLecture.jsx
+++ Frontend/src/components/RegisterForLecture.jsx
@@ -46,13 +46,8 @@ function RegisterLecture() {
 			<div className='max-w-screen-2xl container mx-auto md:px-20 px-6 mb-10'>
 			<div className='mt-28 items-center justify-center text-center'>
 					<h1 className='text-2xl md:text-4xl'>
-						{t('Join Our Event')},{' '}
-						<span className='text-yellow-500'>{t('We Can`t Wait to See You')}!</span>
+						<span className='text-yellow-500'>{t('Conference registration')}!</span>
 					</h1>
-					<p className='mt-12'>
-						{t('Please fill out the form below to send us your message and let us know you`re interested in our event. We will respond to you soon')}!
-
-					</p>
 				</div>
 				{isRegistered ? (
 					<div className='text-center mt-12'>
