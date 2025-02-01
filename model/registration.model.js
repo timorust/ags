1db6730a2d406c9053c94a987337fc002bb78b39
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-02-01 15:21:01 +0000

    model

--- model/registration.model.js
+++ model/registration.model.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose"
 
 const RegistrationSchema = new mongoose.Schema({
-	firstName: String,
-	lastName: String,
-	age: Number,
-	phone: String,
-	lecture: String,
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	age: { type: Number, required: true },
+	phone: { type: String, required: true },
+	lecture: { type: String, required: true },
 	email: { type: String, required: true },
+	identityCard: { type: String, required: true },  
+	finCode: { type: String, required: true },       
 })
 
 export default mongoose.models.Registration ||
