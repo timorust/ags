8e763eac765ab5cd4b2e9872a0db025921392a87
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-02-02 23:34:48 +0000

    g1

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
 		<div>
 			<hr />
 			<footer className='footer footer-center p-10 text-base-content rounded dark:bg-slate-900 dark:text-white'>
-				<nav className="grid grid-flow-col gap-2 overflow-x-auto flex-nowrap">
+				<nav className="grid grid-flow-col gap-1 overflow-x-auto flex-nowrap">
 					<NavLink
 						to="/"
 						className={({ isActive }) =>
