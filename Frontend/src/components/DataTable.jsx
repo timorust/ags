79c55ff4cd54a60f7217ef58dd77c0209e10190d
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-02-02 07:52:26 +0000

    y300

--- Frontend/src/components/DataTable.jsx
+++ Frontend/src/components/DataTable.jsx
@@ -17,7 +17,7 @@ function DataTable() {
           className="flex items-center justify-center h-64 w-64 bg-yellow-500 rounded-full shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer mt-20 dark:bg-slate-900 border-2 border-blue-600 dark:border-blue-800"
           onClick={handleIconClick}
         >
-          <Lightbulb size={120} className="text-white" />
+          <Lightbulb size={120} className="text-yellow-300" />
         </div>
       </div>
 
