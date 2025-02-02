a196fe091ce5e581bd1dfef1468be11620276306
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-02-02 23:28:09 +0000

    lin

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -7,16 +7,7 @@ function Footer() {
 		<div>
 			<hr />
 			<footer className='footer footer-center p-10 text-base-content rounded dark:bg-slate-900 dark:text-white'>
-				{/* <nav className='grid grid-flow-col gap-4'>
-					<Link className='link link-hover text-blue-500 hover:text-blue-600 active:text-blue-700 duration-200' to='/meeting'>{t('Events')}</Link>
-					<Link className='link link-hover text-blue-500 hover:text-blue-600 active:text-blue-700 duration-200' to='/contact'>{t('Contact')}</Link>
-					<Link className='link link-hover text-blue-500 hover:text-blue-600 active:text-blue-700 duration-200' to='/about'>{t('About')}</Link>
-					<Link className='link link-hover text-blue-500 hover:text-blue-600 active:text-blue-700 duration-200' to='/media'>{t('Media')}</Link>
-					<Link className='link link-hover text-blue-500 hover:text-blue-600 active:text-blue-700 duration-200' to='/registration'>{t('Join')}</Link>
-				</nav> */}
-
-
-				<nav className="grid grid-flow-col gap-2 overflow-x-auto">
+				<nav className="grid grid-flow-col gap-2 overflow-x-auto flex-nowrap">
 					<NavLink
 						to="/"
 						className={({ isActive }) =>
