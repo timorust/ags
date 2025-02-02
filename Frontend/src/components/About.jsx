883712e98f83865bda588d98349d78686cd8b93f
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-02-02 16:54:07 +0000

    corr5

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -1,140 +1,84 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import CountdownTimer from './CountdownTimer'
-import { useTranslation } from 'react-i18next'
+import { useEffect, useState } from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import CountdownTimer from './CountdownTimer';
+import { useTranslation } from 'react-i18next';
 
 function About() {
+    const { t } = useTranslation();
+    const [textIndex, setTextIndex] = useState(0);
+    const [imageIndex, setImageIndex] = useState(0);
 
-	const { t } = useTranslation()
-	const [textIndex, setTextIndex] = useState(0)
-	const [imageIndex, setImageIndex] = useState(0)
+    const textContent = [
+        'The Azerbaijan Glaucoma Society (AGS) is an organization that unites glaucoma specialists and shares their clinical and scientific knowledge for the benefit of glaucoma patients, colleagues, and residents.',
+        'The main goal of the AGS is to provide excellent and comprehensive care to all people with glaucoma and those at risk, improve their quality of life, provide a collegial environment for AGS members to exchange ideas and opinions, and jointly defeat glaucoma.',
+        'The organization`s activities include the prevention, early diagnosis, and various treatment methods of glaucoma, a serious, widespread, and blinding disease.',
+        'In addition, the society pays great attention to conducting wide propaganda about glaucoma. AGS was established in 2025.'
+    ];
 
-	const textContent = [
-		'The Azerbaijan Glaucoma Society (AGS) is an organization,that unites glaucoma specialists and shares their clinical, and scientific knowledge for the benefit of glaucoma patients, colleagues and residents.',
-		'The main goal of the AGS is to provide excellent and comprehensive care to all people with glaucoma and those at risk, improve their quality of life, provide a collegial environment for AGS members to exchange ideas and opinions, and jointly defeat glaucoma.',
-		'The organization`s activities include the prevention, early diagnosis and various treatment methods of glaucoma, a serious, widespread and blinding disease.',
-		'In addition, the society pays great attention to conducting wide propaganda about glaucoma. AGS was established in 2025.'
-	]
+    const images = ['/about4.jpg', '/about5.png', '/about2.jpg', '/about3.jpg'];
 
-	const images = [
-		'/about4.jpg',
-		'/about5.png',
-		'/about2.jpg',
-		'/about3.jpg',
-	]
+    useEffect(() => {
+        images.forEach(src => {
+            const img = new Image();
+            img.src = src;
+        });
 
-	useEffect(() => {
-		images.forEach(src => {
-			const img = new Image()
-			img.src = src
-		})
+        const interval = setInterval(() => {
+            setTextIndex(prevIndex => (prevIndex + 1) % textContent.length);
+            setImageIndex(prevIndex => (prevIndex + 1) % images.length);
+        }, 4000);
 
-		const interval = setInterval(() => {
-			setTextIndex(prevIndex => (prevIndex + 1) % textContent.length)
-			setImageIndex(prevIndex => (prevIndex + 1) % images.length)
-		}, 4000)
+        return () => clearInterval(interval);
+    }, [textContent.length, images.length]);
 
-		return () => clearInterval(interval)
-	}, [textContent.length, images.length])
+    return (
+        <>
+            <Navbar />
+            <CountdownTimer targetDate='2025-03-15T09:00:00' />
+            <div className='max-w-screen-lg container mx-auto px-4 md:px-12 mb-8'>
+                <div className='mt-20 text-center mb-8'>
+                    <h1 className='text-xl md:text-3xl'>
+                        {t('AboutTitle')}<span className='text-yellow-500'>!</span>
+                    </h1>
+                </div>
 
-	return (
-		<>
-			<Navbar />
-			<CountdownTimer targetDate='2025-03-15T09:00:00' />
-			<div className='max-w-screen-lg container mx-auto px-4 md:px-12 mb-8'>
-				<div className='mt-20 items-center justify-center text-center mb-8'>
-					<h1 className="text-xl md:text-3xl">
-						{t('AboutTitle')}<span className="text-yellow-500">!</span>
-					</h1>
-				</div>
+                <div className='relative w-full h-48 sm:h-64 lg:h-80 xl:h-96 overflow-hidden rounded-lg shadow-lg flex justify-center items-center'>
+                    {images.map((image, index) => (
+                        <img
+                            key={index}
+                            src={image}
+                            srcSet={`${image}?w=320 320w, ${image}?w=480 480w, ${image}?w=768 768w, ${image}?w=1024 1024w`}
+                            sizes='(max-width: 640px) 100vw, (max-width: 1024px) 80vw, (max-width: 1280px) 70vw, 60vw'
+                            alt={`Image ${index + 1}`}
+                            className={`absolute inset-0 w-full h-full object-cover rounded-lg transition-opacity duration-500 ${index === imageIndex ? 'opacity-100' : 'opacity-0'}`}
+                        />
+                    ))}
+                </div>
 
-				<div className="relative w-full h-48 sm:h-64 lg:h-80 xl:h-96 overflow-hidden rounded-lg shadow-lg sm:mb-8 mb-0 sm:p-2 p-0 flex justify-center items-center">
-					{images.map((image, index) => (
-						<img
-							key={index}
-							src={image}
-							srcSet={` 
-        ${image}?w=320 320w,
-        ${image}?w=480 480w,
-        ${image}?w=768 768w,
-        ${image}?w=1024 1024w
-      `}
-							sizes="(max-width: 640px) 100vw, 
-             (max-width: 1024px) 80vw, 
-             (max-width: 1280px) 70vw, 
-             60vw"
-							alt={`Image ${index + 1}`}
-							className={`absolute inset-0 w-full h-full max-w-[100vw] object-contain rounded-lg transition-all duration-500 ${index === imageIndex ? 'opacity-100' : 'opacity-0'} sm:mb-8 mb-0 lg:mt-8`}
-						/>
-					))}
-				</div>
+                <div className='bg-gray-100 dark:bg-blue-300 p-4 rounded-md shadow-md mb-8 mt-8'>
+                    <div className='text-lg text-gray-500 marquee-container overflow-hidden w-full'>
+                        <div
+                            className='marquee-text inline-block pl-full'
+                            style={{ animation: 'marquee 20s linear infinite' }}
+                        >
+                            {textContent[textIndex]}
+                        </div>
+                    </div>
+                </div>
 
-
-
-				<div className='bg-gray-100 dark:bg-blue-300 p-4 rounded-md shadow-md mb-8 mt-8'>
-					<div
-						className='text-lg text-grey-500 marquee-container'
-						style={{
-							whiteSpace: 'nowrap',
-							overflow: 'hidden',
-							width: '100%',
-						}}
-					>
-						<div
-							className='marquee-text'
-							style={{
-								display: 'inline-block',
-								paddingLeft: '100%',
-								animation: 'marquee 20s linear infinite', // הפחתתי את מהירות האנימציה
-							}}
-						>
-							{textContent[textIndex]}
-						</div>
-					</div>
-				</div>
-
-				<style>
-					{`
-    @keyframes marquee {
-      from {
-        transform: translateX(0%);
-      }
-      to {
-        transform: translateX(-100%);
-      }
-    }
-  `}
-				</style>
-
-
-
-				{/* <div className='bg-gray-100 dark:bg-blue-300 p-4 rounded-md shadow-md mb-8 mt-8'>
-					<div
-						className='text-lg text-grey-500 marquee-container'
-						style={{
-							whiteSpace: 'nowrap',
-							overflow: 'hidden',
-							width: '100%',
-						}}
-					>
-						<div
-							className='marquee-text'
-							style={{
-								display: 'inline-block',
-								paddingLeft: '100%',
-								animation: 'marquee 10s linear infinite',
-							}}
-						>
-							{textContent[textIndex]}
-						</div>
-					</div>
-				</div> */}
-			</div>
-			<Footer />
-		</>
-	)
+                <style>
+                    {`
+                        @keyframes marquee {
+                            from { transform: translateX(100%); }
+                            to { transform: translateX(-100%); }
+                        }
+                    `}
+                </style>
+            </div>
+            <Footer />
+        </>
+    );
 }
 
-export default About
+export default About;
