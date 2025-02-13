7cf477f5840413483daf92844c2813a50b68b1c7
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-02-13 15:39:09 +0000

    corr text

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -9,7 +9,7 @@ function Banner() {
 				<div className='w-full order-2 md:order-1 md:w-1/2 mt-12 md:mt-36'>
 					<div className='space-y-8'>
 						<h1 className='text-2xl md:text-4xl font-bold'>
-						<span className='text-yellow-500'>{t('The Azerbaijan Glaucoma Society')}</span>
+						<span className='text-yellow-500'>{t('Azerbaijan Glaucoma Society')}</span>
 							
 						</h1>
 
