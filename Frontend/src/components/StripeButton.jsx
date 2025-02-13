1cfc1e2af8b75e17d4f3941ff87ad2c7b1ef1cc7
tamir.rustamov <75886852+timorust@users.noreply.github.com>
2025-02-13 13:44:07 +0000

    StripeButton.jsx

---
+++ Frontend/src/components/StripeButton.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+
+const StripeButton = () => {
+	const [product] = useState({
+		name: 'Congress from AGS',
+		price: 100,
+		productBy: 'AGS',
+	})
+
+	const makePayment = token => {
+		fetch(`https://www.ags-az.com/payment`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				token,
+				product,
+			}),
+		})
+			.then(response => response.json())
+			.then(() => {
+				alert('Payment successful!')
+			})
+			.catch(error => {
+				console.error('Payment Error:', error)
+				alert('Payment failed.')
+			})
+	}
+
+	return (
+		<StripeCheckout
+			stripeKey='pk_test_51PMHJLD2fhn4jTSPIzW6eQmeVQHQc6s4S0DH2hCXiKkoV6Q0YZjOCAdSP8iaBPhQR31kZlCUjLjJ4Q7rRPigOZwS00bqewuKwX'
+			token={makePayment}
+			name='By Congress'
+			amount={product.price * 100}
+		>
+			<button className='btn mt-6 bg-green-500 text-white hover:bg-green-700'>
+				Buy now ${product.price}
+			</button>
+		</StripeCheckout>
+	)
+}
+
+export default StripeButton
